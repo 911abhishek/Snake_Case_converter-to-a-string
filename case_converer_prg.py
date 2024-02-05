c005929243ce09f4852f747163b9bcb89b2a0045
911abhishek <138745734+911abhishek@users.noreply.github.com>
2024-02-06 01:23:34 +0530

    Add files via upload

---
+++ case_converer_prg.py
@@ -0,0 +1,26 @@
+    # snake_cased_char_list = []
+    # for char in pascal_or_camel_cased_string:
+    #     if char.isupper():
+    #         converted_character = '_' + char.lower()
+    #         snake_cased_char_list.append(converted_character)
+    #     else:
+    #         snake_cased_char_list.append(char)
+    # snake_cased_string = ''.join(snake_cased_char_list)
+    # clean_snake_cased_string = snake_cased_string.strip('_')
+
+    # return clean_snake_cased_string
+def convert_to_snake_case(pascal_or_camel_cased_string):
+
+    snake_cased_char_list = [
+        '_' + char.lower() if char.isupper()
+        else char
+        for char in pascal_or_camel_cased_string
+    ]
+
+    return ''.join(snake_cased_char_list).strip('_')
+def main():
+    s_ring = input("Enter your string to convert :",)
+    print(convert_to_snake_case(s_ring))
+if __name__ == "__main__":
+    main()
+    
